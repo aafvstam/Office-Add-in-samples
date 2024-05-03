1aa0619c243bf72cd15432aeda91c3472d824106
Soft as in Software <aafvstam@hotmail.com>
2024-05-03 13:56:40 +0200

    Added FluentUI branch to start building FluentUI based Add-ins.

---
+++ Samples/blazor-add-in/Fluent-UI/excel-blazor-add-in/excel-blazor-add-in/Pages/BubbleChart.razor.cs
@@ -0,0 +1,62 @@
+﻿/* Copyright(c) Maarten van Stam. All rights reserved. Licensed under the MIT License. */
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace BlazorAddIn.Pages
+{
+    /// <summary>
+    /// Starter class to demo how to create a bubble chart.
+    /// </summary>
+    [SupportedOSPlatform("browser")]
+    public partial class BubbleChart
+    {
+        [Inject, AllowNull]
+        private IJSRuntime JSRuntime { get; set; }
+        private IJSObjectReference? JSModule { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/BubbleChart.razor.js");
+            }
+        }
+
+        /// <summary>
+        /// Function to create the starter table as source for the bubble chart.
+        /// </summary>
+        private async Task CreateTable() =>
+            await JSModule.InvokeVoidAsync("createTable");
+
+        /// <summary>
+        /// Function to create the actual bubble chart.
+        /// </summary>
+        private async Task CreateBubbleChart() => 
+            await JSModule.InvokeVoidAsync("createBubbleChart");
+
+        [JSImport("createTable", "BubbleChart")]
+        internal static partial Task CreateImportedTable();
+
+        [JSImport("createBubbleChart", "BubbleChart")]
+        internal static partial Task RunCreateChart();
+
+        [JSInvokable]
+        public static async Task CreateBubbles()
+        {
+            await JSHost.ImportAsync("BubbleChart", "../Pages/BubbleChart.razor.js");
+            await CreateImportedTable();
+            await RunCreateChart();
+        }
+
+        [JSInvokable]
+        public static string SayHelloBubble(string name)
+        {
+            return $"Hello Bubble, {name} from BubbleChart!";
+        }
+    }
+}
+
---
+++ Samples/blazor-add-in/Fluent-UI/excel-blazor-add-in/excel-blazor-add-in/Pages/Index.razor.cs
@@ -0,0 +1,42 @@
+﻿/* Copyright(c) Maarten van Stam. All rights reserved. Licensed under the MIT License. */
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorAddIn.Pages
+{
+    /// <summary>
+    /// Starter class to demo how to insert `Hello world!` text.
+    /// </summary>
+    public partial class Index
+    {
+        [Inject, AllowNull]
+        private IJSRuntime JSRuntime { get; set; } 
+        private IJSObjectReference JSModule { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Debug.WriteLine("Hit OnAfterRenderAsync in Index.razor.cs!");
+                Console.WriteLine("Hit OnAfterRenderAsync in Index.razor.cs in Console!");
+                JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/Index.razor.js");
+            }
+        }
+
+        /// <summary>
+        /// Basic function to invoke inserting `Hello world!` text.
+        /// </summary>
+        private async Task HelloButton() =>
+            await JSModule.InvokeVoidAsync("helloButton");
+
+        [JSInvokable]
+        public static string SayHelloIndex(string name)
+        {
+            return $"Hello Index, {name} from Index!";
+        }
+    }
+}
